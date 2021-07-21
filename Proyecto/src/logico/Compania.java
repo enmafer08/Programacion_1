c7bb1f3f26b07117f6423a36f419cc22bf5dab71
manue <manue@DESKTOP-UUEHP73>
2021-07-21 00:44:00 -0400

    Creacion de las primeras clases

---
+++ Proyecto/src/logico/Compania.java
@@ -0,0 +1,109 @@
+package logico;
+
+public class Compania {
+	
+	private String rnc;
+	private String nombre;
+	private String localidad;
+	private String direccion;
+	private String provincia;
+	private String ciudad;
+	private String telefono;
+	private String correo;
+	
+	
+	public Compania(String nombre, String localidad, String direccion, String provincia, String ciudad, String telefono,
+			String correo) {
+		super();
+		this.nombre = nombre;
+		this.localidad = localidad;
+		this.direccion = direccion;
+		this.provincia = provincia;
+		this.ciudad = ciudad;
+		this.telefono = telefono;
+		this.correo = correo;
+	}
+
+
+	public String getRnc() {
+		return rnc;
+	}
+
+
+	public void setRnc(String rnc) {
+		this.rnc = rnc;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getCorreo() {
+		return correo;
+	}
+
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	
+
+}
