da809e8eedbabf930b2797591cf22379539976cd
manue <manue@DESKTOP-UUEHP73>
2021-07-21 01:15:07 -0400

    En el push anterior olvide añadir los cambios

---
+++ Proyecto/src/logico/CentroDeTrabajo.java
@@ -0,0 +1,75 @@
+package logico;
+
+import java.util.List;
+
+public class CentroDeTrabajo {
+
+	private List<Compania> micompania;
+	private List<Persona> mipersona;
+	private List<SolicitudEmpleado> misolicitud_e;
+	private List<SolicitudCompania> misolicitud_c;
+	private static CentroDeTrabajo centrodetrabajo = null;
+	
+	
+	private CentroDeTrabajo(List<Compania> micompania, List<Persona> mipersona, List<SolicitudEmpleado> misolicitud_e,
+			List<SolicitudCompania> misolicitud_c) {
+		super();
+		this.micompania = micompania;
+		this.mipersona = mipersona;
+		this.misolicitud_e = misolicitud_e;
+		this.misolicitud_c = misolicitud_c;
+	}
+
+
+	public List<Compania> getMicompania() {
+		return micompania;
+	}
+
+
+	public void setMicompania(List<Compania> micompania) {
+		this.micompania = micompania;
+	}
+
+
+	public List<Persona> getMipersona() {
+		return mipersona;
+	}
+
+
+	public void setMipersona(List<Persona> mipersona) {
+		this.mipersona = mipersona;
+	}
+
+
+	public List<SolicitudEmpleado> getMisolicitud_e() {
+		return misolicitud_e;
+	}
+
+
+	public void setMisolicitud_e(List<SolicitudEmpleado> misolicitud_e) {
+		this.misolicitud_e = misolicitud_e;
+	}
+
+
+	public List<SolicitudCompania> getMisolicitud_c() {
+		return misolicitud_c;
+	}
+
+
+	public void setMisolicitud_c(List<SolicitudCompania> misolicitud_c) {
+		this.misolicitud_c = misolicitud_c;
+	}
+
+
+	public static CentroDeTrabajo getCentrodetrabajo() {
+		return centrodetrabajo;
+	}
+
+
+	public static void setCentrodetrabajo(CentroDeTrabajo centrodetrabajo) {
+		CentroDeTrabajo.centrodetrabajo = centrodetrabajo;
+	}
+	
+	
+	
+}
---
+++ Proyecto/src/logico/SolicitudCompania.java
@@ -0,0 +1,162 @@
+package logico;
+
+import java.util.List;
+
+public class SolicitudCompania {
+
+	private String id;
+	private Compania compania;
+	private String tipo_de_empleado;
+	private String habilidades_requeridas;
+	private int cantidad;
+	private float salario_minimo;
+	private boolean estado;
+	private List<String> lenguajes;
+	private boolean viajes;
+	private boolean movimientos;
+	private boolean licencia;
+	private int horas_de_trabajo;
+	public static int cod = 0;
+	
+	
+	public SolicitudCompania(String id, Compania compania, String tipo_de_empleado, String habilidades_requeridas,
+			int cantidad, float salario_minimo, boolean estado, List<String> lenguajes, boolean viajes,
+			boolean movimientos, boolean licencia, int horas_de_trabajo) {
+		super();
+		this.id = id;
+		this.compania = compania;
+		this.tipo_de_empleado = tipo_de_empleado;
+		this.habilidades_requeridas = habilidades_requeridas;
+		this.cantidad = cantidad;
+		this.salario_minimo = salario_minimo;
+		this.estado = estado;
+		this.lenguajes = lenguajes;
+		this.viajes = viajes;
+		this.movimientos = movimientos;
+		this.licencia = licencia;
+		this.horas_de_trabajo = horas_de_trabajo;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public Compania getCompania() {
+		return compania;
+	}
+
+
+	public void setCompania(Compania compania) {
+		this.compania = compania;
+	}
+
+
+	public String getTipo_de_empleado() {
+		return tipo_de_empleado;
+	}
+
+
+	public void setTipo_de_empleado(String tipo_de_empleado) {
+		this.tipo_de_empleado = tipo_de_empleado;
+	}
+
+
+	public String getHabilidades_requeridas() {
+		return habilidades_requeridas;
+	}
+
+
+	public void setHabilidades_requeridas(String habilidades_requeridas) {
+		this.habilidades_requeridas = habilidades_requeridas;
+	}
+
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+	public float getSalario_minimo() {
+		return salario_minimo;
+	}
+
+
+	public void setSalario_minimo(float salario_minimo) {
+		this.salario_minimo = salario_minimo;
+	}
+
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+
+	public List<String> getLenguajes() {
+		return lenguajes;
+	}
+
+
+	public void setLenguajes(List<String> lenguajes) {
+		this.lenguajes = lenguajes;
+	}
+
+
+	public boolean isViajes() {
+		return viajes;
+	}
+
+
+	public void setViajes(boolean viajes) {
+		this.viajes = viajes;
+	}
+
+
+	public boolean isMovimientos() {
+		return movimientos;
+	}
+
+
+	public void setMovimientos(boolean movimientos) {
+		this.movimientos = movimientos;
+	}
+
+
+	public boolean isLicencia() {
+		return licencia;
+	}
+
+
+	public void setLicencia(boolean licencia) {
+		this.licencia = licencia;
+	}
+
+
+	public int getHoras_de_trabajo() {
+		return horas_de_trabajo;
+	}
+
+
+	public void setHoras_de_trabajo(int horas_de_trabajo) {
+		this.horas_de_trabajo = horas_de_trabajo;
+	}
+	
+	
+	
+}
