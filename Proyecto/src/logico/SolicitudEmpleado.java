43d3114078aa5d0d10339bbeda4ef5acb2cd97ce
manue <manue@DESKTOP-UUEHP73>
2021-07-21 01:29:47 -0400

    Agregando clases

--- Proyecto/src/logico/SolicitudEmpleado.java
+++ Proyecto/src/logico/SolicitudEmpleado.java
@@ -6,7 +6,7 @@ public class SolicitudEmpleado {
 
 	private String id;
 	private Persona aplicante;
-	private boolean estado;
+	private boolean estadocivil;
 	private float salariominimo;
 	private List<String> lenguajes;
 	private int hrsdetrabajo;
@@ -14,119 +14,99 @@ public class SolicitudEmpleado {
 	private boolean dispmudarse;
 	private boolean licencia;
 	public static int cod = 0;
-	
-	
-	public SolicitudEmpleado(String id, Persona aplicante, boolean estado, float salariominimo, List<String> lenguajes,
-			int hrsdetrabajo, boolean dispviajar, boolean dispmudarse, boolean licencia) {
-		super();
-		this.id = id;
-		this.aplicante = aplicante;
-		this.estado = estado;
-		this.salariominimo = salariominimo;
-		this.lenguajes = lenguajes;
-		this.hrsdetrabajo = hrsdetrabajo;
-		this.dispviajar = dispviajar;
-		this.dispmudarse = dispmudarse;
-		this.licencia = licencia;
-	}
-
 
 	public String getId() {
 		return id;
 	}
 
-
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
-	public Persona getAplicante() {
+	public person getAplicante() {
 		return aplicante;
 	}
 
-
-	public void setAplicante(Persona aplicante) {
+	public void setAplicante(person aplicante) {
 		this.aplicante = aplicante;
 	}
 
-
-	public boolean isEstado() {
-		return estado;
+	public boolean isEstadocivil() {
+		return estadocivil;
 	}
 
-
-	public void setEstado(boolean estado) {
-		this.estado = estado;
+	public void setEstadocivil(boolean estadocivil) {
+		this.estadocivil = estadocivil;
 	}
-
 
 	public float getSalariominimo() {
 		return salariominimo;
 	}
 
-
 	public void setSalariominimo(float salariominimo) {
 		this.salariominimo = salariominimo;
 	}
 
-
-	public List<String> getLenguajes() {
+	public list<string> getLenguajes() {
 		return lenguajes;
 	}
 
-
-	public void setLenguajes(List<String> lenguajes) {
+	public void setLenguajes(list<string> lenguajes) {
 		this.lenguajes = lenguajes;
 	}
-
 
 	public int getHrsdetrabajo() {
 		return hrsdetrabajo;
 	}
 
-
 	public void setHrsdetrabajo(int hrsdetrabajo) {
 		this.hrsdetrabajo = hrsdetrabajo;
 	}
-
 
 	public boolean isDispviajar() {
 		return dispviajar;
 	}
 
-
 	public void setDispviajar(boolean dispviajar) {
 		this.dispviajar = dispviajar;
 	}
-
 
 	public boolean isDispmudarse() {
 		return dispmudarse;
 	}
 
-
 	public void setDispmudarse(boolean dispmudarse) {
 		this.dispmudarse = dispmudarse;
 	}
-
 
 	public boolean isLicencia() {
 		return licencia;
 	}
 
-
 	public void setLicencia(boolean licencia) {
 		this.licencia = licencia;
 	}
-
 
 	public static int getCod() {
 		return cod;
 	}
 
-
 	public static void setCod(int cod) {
 		SolicitudEmpleado.cod = cod;
 	}
+
+	private SolicitudEmpleado(String id, Persona aplicante, boolean estadocivil, float salariominimo,
+			List<String> lenguajes, int hrsdetrabajo, boolean dispviajar, boolean dispmudarse, boolean licencia) {
+		super();
+		this.id = id;
+		this.aplicante = aplicante;
+		this.estadocivil = estadocivil;
+		this.salariominimo = salariominimo;
+		this.lenguajes = lenguajes;
+		this.hrsdetrabajo = hrsdetrabajo;
+		this.dispviajar = dispviajar;
+		this.dispmudarse = dispmudarse;
+		this.licencia = licencia;
+	}
+
 }
