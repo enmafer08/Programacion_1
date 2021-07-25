4f277d31d798572467293fe13677413dad20e45a
fermi <fermi@LAPTOP-M5OD2SNC>
2021-07-24 22:34:20 -0400

    AGREGANDO METODOS EN LA CLASE PRINCIPAL

--- Proyecto/src/logico/CentroDeTrabajo.java
+++ Proyecto/src/logico/CentroDeTrabajo.java
@@ -74,6 +74,120 @@ public class CentroDeTrabajo implements Serializable {
 		CentroDeTrabajo.centrodetrabajo = centrodetrabajo;
 	}
 	
+	public void agregarcompania (Compania comp)
+	{
+		this.micompania.add(comp);
+	}
+	
+	public void modifcompania (Compania aux, Compania modif)
+	{
+		int pos = micompania.indexOf(modif);
+		micompania.add(pos, aux);
+		micompania.remove(modif);
+	}
+
+	public void agregarpersona (Persona persona)
+	{
+		this.mipersona.add(persona);
+	}
+	
+	public void agregarempleadosolic(SolicitudEmpleado s)
+	{
+		this.misolicitud_e.add(s);
+	}
+	
+	public void agregarcompaniasolic(SolicitudCompania sc)
+	{
+		this.misolicitud_c.add(sc);
+	}
+	
+	public Compania buscarcompaniaporid(String id)
+	{
+		for (Compania a : micompania)
+		{
+			if(a.getRnc().equalsIgnoreCase(id))
+			{
+				return a;
+			}
+		}
+		return null;
+	}
+	
+	public Persona buscarpersonaporid (String id)
+	{
+		for(Persona b : mipersona)
+		{
+			if (b.getId().equalsIgnoreCase(id))
+			{
+				return b;
+		}
+		}
+		return null;
+	}
+	
+	public SolicitudCompania buscarsolicitudcompID(String id)
+	{
+		for (SolicitudCompania c : misolicitud_c)
+		{
+			if(c.getId().equalsIgnoreCase(id))
+			{
+				return c;
+			}
+			}
+		return null;
+	}
+	
+	public SolicitudEmpleado buscarsolicitudempID(String id)
+	{
+		for(SolicitudEmpleado d : misolicitud_e)
+		{
+			if(d.getId().equalsIgnoreCase(id))
+			{
+				return d;
+			}
+			}
+		return null;
+	}
+	
+	public List<SolicitudCompania>getSolicitudCompania(Compania c)
+	{
+		List<SolicitudCompania>solicitud = new ArrayList<SolicitudCompania>();
+		for(SolicitudCompania sc : misolicitud_c)
+		{
+			if(sc.getCompania() == c && sc.isEstado())
+			{
+				solicitud.add(sc);
+			}
+		}
+		return solicitud;
+	}
+	
+	public List<Persona> getEst()
+	{
+		List<Persona> allEst = new ArrayList<>();
+		for (Persona e : mipersona)
+		{
+			if(e instanceof Estudiante)
+			{
+				allEst.add((Estudiante) e);
+			}
+		}
+		return allEst;
+	}
+	
+	public List<Persona> getTec()
+	{
+		List<Persona> allTec = new ArrayList<>();
+		for (Persona f : mipersona)
+		{
+			if(f instanceof Tecnico)
+			{
+				allTec.add((Tecnico) f);
+			}
+		}
+		return allTec;
+	}
+	
 	
 	
 }
